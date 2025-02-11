ce1da858b48fd8fad5cba5aa20ec3476f332291f
Enes Kanbur <eneskanbur02@hotmail.com>
2025-02-12 01:23:52 +0300

    Basic dashboard UI design and some resources file are created.

---
+++ app/src/main/java/com/example/saglamisapp/MainSaglamIs.kt
@@ -0,0 +1,13 @@
+package com.example.saglamisapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainSaglamIs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_main_saglam_is)
+    }
+}
